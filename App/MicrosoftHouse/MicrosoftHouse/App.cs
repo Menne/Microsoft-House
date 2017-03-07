67dd5383ad2de4349e8a5245b49e8b6074dbcdf8
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-07 17:42:33 +0100

    Bellona

--- App/MicrosoftHouse/MicrosoftHouse/App.cs
+++ App/MicrosoftHouse/MicrosoftHouse/App.cs
@@ -21,7 +21,7 @@ namespace MicrosoftHouse
                     Children = {
                         new Label {
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                            Text = "Welcome to Menne and Filo APP!"
                         }
                     }
                 }
