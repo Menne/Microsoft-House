8de39867354ef7d50342a59684eb80b476bd8d00
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-08 20:08:02 +0100

    Click e Render CrossPlatform Funzionante

--- App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
+++ App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-
 using Xamarin.Forms;
 
 namespace MicrosoftHouse
 {
 	public partial class MainPage : ContentPage
 	{
+
+		string translatedNumber;
+
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		void Translate(object sender, EventArgs e)
+		{
+			translatedNumber = phoneNumberText.Text;
+			if (!string.IsNullOrWhiteSpace(translatedNumber))
+			{
+				callButton.IsEnabled = true;
+				callButton.Text = "Call " + translatedNumber;
+			}
+			else
+			{
+				callButton.IsEnabled = false;
+				callButton.Text = "Call";
+			}
 		}
 	}
 }
