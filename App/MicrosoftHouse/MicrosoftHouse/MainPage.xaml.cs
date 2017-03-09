f9eba962fc2252d9901c9e4e219b8c269ef1a1de
Emanuele Torelli <mennetorelli@hotmail.it>
2017-03-09 13:17:39 +0100

    aggiunto UWP

--- App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
+++ App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
@@ -1,36 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MicrosoftHouse
 {
-	public partial class MainPage : ContentPage
-	{
-
-		string translatedNumber;
-
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-
-		void Translate(object sender, EventArgs e)
-		{
-			translatedNumber = phoneNumberText.Text;
-			if (!string.IsNullOrWhiteSpace(translatedNumber))
-			{
-				callButton.IsEnabled = true;
-				callButton.Text = "Call " + translatedNumber;
-			}
-			else
-			{
-				callButton.IsEnabled = false;
-				callButton.Text = "Call";
-			}
-		}
-
-		async void OnCallHistory(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new SecondaryPage());
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
 }
