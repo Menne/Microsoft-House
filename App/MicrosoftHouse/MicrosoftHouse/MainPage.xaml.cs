91c8466f34842d77d1dfe68a30a89d1ca4402e80
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-10 18:53:20 +0100

    Tolte Classi di Prova

--- App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
+++ App/MicrosoftHouse/MicrosoftHouse/MainPage.xaml.cs
@@ -15,29 +15,6 @@ namespace MicrosoftHouse
 
         }
 
-		string translatedNumber;
-
-		void Translate(object sender, EventArgs e)
-		{
-			translatedNumber = phoneNumberText.Text;
-			if (!string.IsNullOrWhiteSpace(translatedNumber))
-			{
-				callButton.IsEnabled = true;
-				callButton.Text = "Call " + translatedNumber;
-			}
-			else
-			{
-				callButton.IsEnabled = false;
-				callButton.Text = "Call";
-			}
-		}
-
-
-		async void OnCallHistory(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new SecondaryPage());
-		}
-
 		async void OnRoomList(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new RoomList());
