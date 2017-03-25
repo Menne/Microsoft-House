e20cb3b7b30aefc236873cf5eb70abc274e44466
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-25 19:52:45 +0100

    Hamburgerrrrr
    Former-commit-id: 62376e1f96b861fdc65daa5b512bc34a3e7c2e41

---
+++ App/MicrosoftHouse/MicrosoftHouse/Pages/MasterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MicrosoftHouse
+{
+	public partial class MasterPage : MasterDetailPage
+	{
+		public MasterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- App/MicrosoftHouse/MicrosoftHouse/ViewModels/EntryPageViewModel.cs
+++ App/MicrosoftHouse/MicrosoftHouse/ViewModels/EntryPageViewModel.cs
@@ -26,12 +26,7 @@ namespace MicrosoftHouse.ViewModels
 			try
 			{
 				// Creando una nuova navigation page, non cè più la possibilità di tornare indietro.
-				Application.Current.MainPage = new NavigationPage(new AllRoomsPage())
-				{
-					// Colore della action bar della navigation page
-					BarBackgroundColor = Color.Aqua,
-					BarTextColor = Color.Blue
-				};
+				Application.Current.MainPage = new MasterPage();
 			}
 			catch (Exception ex)
 			{
