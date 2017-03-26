f72b22636c8c842222705ae1e8aa35b66375b244
Emanuele Torelli <mennetorelli@hotmail.it>
2017-03-26 15:21:11 +0200

    carousel che non vuole andare
    Former-commit-id: ab688a4aa8b617ed3636d88f3d2896d061c693e3

--- App/MicrosoftHouse/MicrosoftHouse/ViewModels/CarParkViewModel.cs
+++ App/MicrosoftHouse/MicrosoftHouse/ViewModels/CarParkViewModel.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MicrosoftHouse.ViewModels
 {
     class CarParkViewModel : ViewModelBase
     {
         int parkingSpaces=25, distance=3, timeToArrival=15;
+
+        public ObservableCollection<Label> ItemsForCarousel { get; set; }
+
+        public CarParkViewModel()
+        {
+            ItemsForCarousel = new ObservableCollection<Label>
+            {
+                new Label
+                {
+                    Text="carousel1"
+                },
+                new Label
+                {
+                    Text="carousel2"
+                },
+                new Label
+                {
+                    Text="carousel3"
+                }
+            };
+        }
 
         public int ParkingSpaces
         {
