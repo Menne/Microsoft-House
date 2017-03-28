f602b9a20c2763485a7c5b48b176f279dd284cb6
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-28 16:48:07 +0200

    Extended Entry Fatto
    Former-commit-id: 7851210aaa9f17c440fcc4645dc266e976543a23

---
+++ App/MicrosoftHouse/MicrosoftHouse.iOS/ExtendedEntryRender.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using MicrosoftHouse;
+using MicrosoftHouse.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRender))]
+namespace MicrosoftHouse.iOS
+{
+	public class ExtendedEntryRender : EntryRenderer
+	{
+			/// <summary>
+			/// The on element changed callback.
+			/// </summary>
+			/// <param name="e">The event arguments.</param>
+			protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+			{
+				base.OnElementChanged(e);
+
+				var view = e.NewElement as ExtendedEntry;
+
+				if (view != null)
+				{
+					Control.BorderStyle = UITextBorderStyle.None;
+
+					SetFont(view);
+					SetTextAlignment(view);
+					SetPlaceholderTextColor(view);
+					SetMaxLength(view);
+
+					//ResizeHeight();
+				}
+			}
+
+			/// <summary>
+			/// The on element property changed callback
+			/// </summary>
+			/// <param name="sender">The sender.</param>
+			/// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
+			protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+			{
+				base.OnElementPropertyChanged(sender, e);
+
+				var view = (ExtendedEntry)Element;
+
+				if (e.PropertyName == ExtendedEntry.FontProperty.PropertyName)
+					SetFont(view);
+				if (e.PropertyName == ExtendedEntry.XAlignProperty.PropertyName)
+					SetTextAlignment(view);
+				if (e.PropertyName == ExtendedEntry.PlaceholderTextColorProperty.PropertyName)
+					SetPlaceholderTextColor(view);
+
+				//ResizeHeight();
+			}
+
+			/// <summary>
+			/// Sets the text alignment.
+			/// </summary>
+			/// <param name="view">The view.</param>
+			private void SetTextAlignment(ExtendedEntry view)
+			{
+				switch (view.XAlign)
+				{
+					case TextAlignment.Center:
+						Control.TextAlignment = UITextAlignment.Center;
+						break;
+					case TextAlignment.End:
+						Control.TextAlignment = UITextAlignment.Right;
+						break;
+					case TextAlignment.Start:
+						Control.TextAlignment = UITextAlignment.Left;
+						break;
+				}
+			}
+
+			/// <summary>
+			/// Sets the font.
+			/// </summary>
+			/// <param name="view">The view.</param>
+			private void SetFont(ExtendedEntry view)
+			{
+				UIFont uiFont;
+				if (view.Font != Font.Default && (uiFont = view.Font.ToUIFont()) != null)
+					Control.Font = uiFont;
+				else if (view.Font == Font.Default)
+					Control.Font = UIFont.SystemFontOfSize(17f);
+			}
+			/// <summary>
+			/// Sets the maxLength.
+			/// </summary>
+			/// <param name="view">The view.</param>
+			private void SetMaxLength(ExtendedEntry view)
+			{
+				Control.ShouldChangeCharacters = (textField, range, replacementString) =>
+				{
+					var newLength = textField.Text.Length + replacementString.Length - range.Length;
+					return newLength <= view.MaxLength;
+				};
+			}
+
+			/// <summary>
+			/// Resizes the height.
+			/// </summary>
+			private void ResizeHeight()
+			{
+				if (Element.HeightRequest >= 0) return;
+
+				var height = Math.Max(Bounds.Height,
+					new UITextField { Font = Control.Font }.IntrinsicContentSize.Height);
+
+				Control.Frame = new CGRect(0.0f, 0.0f, (nfloat)Element.Width, (nfloat)height);
+
+				Element.HeightRequest = height;
+			}
+
+			/// <summary>
+			/// Sets the color of the placeholder text.
+			/// </summary>
+			/// <param name="view">The view.</param>
+			void SetPlaceholderTextColor(ExtendedEntry view)
+			{
+				/*
+	UIColor *color = [UIColor lightTextColor];
+	YOURTEXTFIELD.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"PlaceHolder Text" attributes:@{NSForegroundColorAttributeName: color}];
+				*/
+				if (string.IsNullOrEmpty(view.Placeholder) == false && view.PlaceholderTextColor != Color.Default)
+				{
+					NSAttributedString placeholderString = new NSAttributedString(view.Placeholder, new UIStringAttributes() { ForegroundColor = view.PlaceholderTextColor.ToUIColor() });
+					Control.AttributedPlaceholder = placeholderString;
+				}
+			}
+
+	}
+}
---
+++ App/MicrosoftHouse/MicrosoftHouse/Helpers/ExtendedEntry.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MicrosoftHouse
+{
+	public class ExtendedEntry : Entry
+	{
+		/// <summary>
+		/// The font property
+		/// </summary>
+		public static readonly BindableProperty FontProperty =
+			BindableProperty.Create("Font", typeof(Font), typeof(ExtendedEntry), new Font());
+
+		/// <summary>
+		/// The XAlign property
+		/// </summary>
+		public static readonly BindableProperty XAlignProperty =
+			BindableProperty.Create("XAlign", typeof(TextAlignment), typeof(ExtendedEntry),
+			TextAlignment.Start);
+
+		/// <summary>
+		/// The PlaceholderTextColor property
+		/// </summary>
+		public static readonly BindableProperty PlaceholderTextColorProperty =
+			BindableProperty.Create("PlaceholderTextColor", typeof(Color), typeof(ExtendedEntry), Color.Default);
+
+		/// <summary>
+		/// The MaxLength property
+		/// </summary>
+		public static readonly BindableProperty MaxLengthProperty =
+			BindableProperty.Create("MaxLength", typeof(int), typeof(ExtendedEntry), int.MaxValue);
+
+		/// <summary>
+		/// Gets or sets the MaxLength
+		/// </summary>
+		public int MaxLength
+		{
+			get { return (int)this.GetValue(MaxLengthProperty); }
+			set { this.SetValue(MaxLengthProperty, value); }
+		}
+
+		/// <summary>
+		/// Gets or sets the Font
+		/// </summary>
+		public Font Font
+		{
+			get { return (Font)GetValue(FontProperty); }
+			set { SetValue(FontProperty, value); }
+		}
+
+		/// <summary>
+		/// Gets or sets the X alignment of the text
+		/// </summary>
+		public TextAlignment XAlign
+		{
+			get { return (TextAlignment)GetValue(XAlignProperty); }
+			set { SetValue(XAlignProperty, value); }
+		}
+
+		/// <summary>
+		/// Sets color for placeholder text
+		/// </summary>
+		public Color PlaceholderTextColor
+		{
+			get { return (Color)GetValue(PlaceholderTextColorProperty); }
+			set { SetValue(PlaceholderTextColorProperty, value); }
+		}
+
+	}
+}
