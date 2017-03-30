a778757005759cdd7045658f50234580dc4cd787
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-03-30 15:50:38 +0200

    Renderer aggiunti
    Former-commit-id: 1d55ac06c514add81f5d83150bfe87e3fc987ada

---
+++ App/MicrosoftHouse/MicrosoftHouse.Android/ExtendedDatePickerRender.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MicrosoftHouse;
+using MicrosoftHouse.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedDatePicker), typeof(ExtendedDatePickerRender))]
+namespace MicrosoftHouse.Droid
+{
+	public class ExtendedDatePickerRender : DatePickerRenderer
+	{
+		//PUSH - Prova
+		protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			ExtendedDatePicker datePicker = (ExtendedDatePicker)Element;
+
+			if (datePicker != null)
+			{
+				SetTextColor(datePicker);
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (Control == null)
+			{
+				return;
+			}
+
+			ExtendedDatePicker datePicker = (ExtendedDatePicker)Element;
+
+			if (e.PropertyName == ExtendedDatePicker.TextColorProperty.PropertyName)
+			{
+				this.Control.SetTextColor(datePicker.TextColor.ToAndroid());
+			}
+		}
+
+		void SetTextColor(ExtendedDatePicker datePicker)
+		{
+			this.Control.SetTextColor(datePicker.TextColor.ToAndroid());
+		}
+	}
+}
---
+++ App/MicrosoftHouse/MicrosoftHouse.iOS/ExtendedDatePickerRender.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using MicrosoftHouse;
+using MicrosoftHouse.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRendererAttribute(typeof(ExtendedDatePicker), typeof(ExtendedDatePickerRender))]
+namespace MicrosoftHouse.iOS
+{
+	public class ExtendedDatePickerRender : DatePickerRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			ExtendedDatePicker datePicker = (ExtendedDatePicker)Element;
+
+			if (datePicker != null)
+			{
+				Control.BorderStyle = UITextBorderStyle.None;
+				//Control.BorderStyle = UITextBorderStyle.None;
+				SetPlaceholder(datePicker);
+				SetFont(datePicker);
+				//SetBorderStyle(datePicker);
+				SetTextColor(datePicker);
+			}
+
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (Control == null)
+			{
+				return;
+			}
+
+			ExtendedDatePicker datePicker = (ExtendedDatePicker)Element;
+			if (e.PropertyName == ExtendedEntry.FontProperty.PropertyName)
+			{
+				SetFont(datePicker);
+			}
+			if (e.PropertyName == ExtendedDatePicker.TextColorProperty.PropertyName)
+			{
+				this.Control.TextColor = datePicker.TextColor.ToUIColor();
+			}
+		}
+
+		private void SetFont(ExtendedDatePicker datePicker)
+		{
+			UIFont uiFont;
+			if (datePicker.Font != Font.Default && (uiFont = datePicker.Font.ToUIFont()) != null)
+				Control.Font = uiFont;
+			else if (datePicker.Font == Font.Default)
+				Control.Font = UIFont.SystemFontOfSize(17f);
+		}
+
+		void SetTextColor(ExtendedDatePicker datePicker)
+		{
+			this.Control.TextColor = datePicker.TextColor.ToUIColor();
+		}
+
+		void SetPlaceholder(ExtendedDatePicker datePicker)
+		{
+			this.Control.Text = datePicker.PlaceHolder;
+		}
+	}
+}
