6ef099d65e0e7500ff3671a25dd94b0550c2dd0d
Emanuele Torelli <mennetorelli@hotmail.it>
2017-04-07 12:04:22 +0200

    Barchart si vola parte 2

--- App/MicrosoftHouse/MicrosoftHouse/Pages/CarParkPage.xaml.cs
+++ App/MicrosoftHouse/MicrosoftHouse/Pages/CarParkPage.xaml.cs
@@ -12,27 +12,7 @@ namespace MicrosoftHouse
 		{
 			InitializeComponent();
             BindingContext = new CarParkViewModel();
-
-    //        InitializeStatistics();
         }
 
-   /*     private void InitializeStatistics()
-        {
-            List<BoxView> barChart = new List<BoxView>();
-            for (int i = 0; i < 13; i++)
-            {
-                BoxView boxView = new BoxView
-                {
-                    Color = Color.FromHex("FF01A4EF"),
-                    HeightRequest,
-                    VerticalOptions = LayoutOptions.End,
-                };
-                boxView.SetBinding(HeightRequest)
-                barChart.Add(boxView);
-            }
-            StatisticsGrid.Children.AddHorizontal(barChart);
-
-            } 
-        }  */
     }
 }
