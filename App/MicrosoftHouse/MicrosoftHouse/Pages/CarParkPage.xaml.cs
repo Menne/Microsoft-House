45c7bd5c75201e49b17863193806208b3e90a4aa
Emanuele Torelli <mennetorelli@hotmail.it>
2017-04-07 16:34:14 +0200

    migliorato xaml

--- App/MicrosoftHouse/MicrosoftHouse/Pages/CarParkPage.xaml.cs
+++ App/MicrosoftHouse/MicrosoftHouse/Pages/CarParkPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using MicrosoftHouse.ViewModels;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace MicrosoftHouse
@@ -12,6 +12,34 @@ namespace MicrosoftHouse
 		{
 			InitializeComponent();
             BindingContext = new CarParkViewModel();
+
+            InitializeStatistics();
+        }
+
+        private void InitializeStatistics()
+        {
+       /*     for (int i = 0; i < 12; i++)
+            {
+                BoxView boxView = new BoxView
+                {
+                    Color = Color.FromHex("FF01A4EF"),
+                    VerticalOptions = LayoutOptions.End,
+                };
+                boxView.SetBinding(HeightRequestProperty, new Binding("SelectedDayStatistics", BindingMode.TwoWay, source: BindingContext));
+                StatisticsGrid.Children.Add(boxView, i + 1, 0);
+            }    */
+
+            for (int i = 0; i < 13; i++)
+            {
+                Label label = new Label
+                {
+                    Text = (i+8).ToString(),
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                };
+                StatisticsGrid.Children.Add(label, i, 1);
+                Grid.SetColumnSpan(label, 2);
+            }
         }
 
         public void OnPressed(object sender, EventArgs e)
