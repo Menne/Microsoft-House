8d29d7aaa3564c243f89b2146d3141dc3dc73fa6
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-04-10 10:21:20 +0200

    mssing elements

---
+++ App/MicrosoftHouse/MicrosoftHouse/Helpers/ExtendedPicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MicrosoftHouse
+{
+	public class ExtendedPicker : Picker
+	{
+		public static readonly BindableProperty TextColorProperty =
+			BindableProperty.Create("TextColor", typeof(Color), typeof(ExtendedDatePicker), Color.Default);
+
+		public Color TextColor
+		{
+		get { return (Color)GetValue(TextColorProperty); }
+		set { SetValue(TextColorProperty, value); }
+		}
+
+		/// <summary>
+		/// The font property
+		/// </summary>
+		public static readonly BindableProperty FontProperty =
+			BindableProperty.Create("Font", typeof(Font), typeof(ExtendedDatePicker), new Font());
+
+		/// <summary>
+		/// Gets or sets the Font
+		/// </summary>
+		public Font Font
+		{
+		get { return (Font)GetValue(FontProperty); }
+		set { SetValue(FontProperty, value); }
+		}
+
+		public static readonly BindableProperty PlaceHolderProperty = BindableProperty.Create(
+		   "PlaceHolder",
+		   typeof(string),
+		   typeof(ExtendedDatePicker),
+		   "");
+
+		public string PlaceHolder
+		{
+			get { return (string)GetValue(PlaceHolderProperty); }
+			set { SetValue(PlaceHolderProperty, value); }
+		}
+	}
+}
