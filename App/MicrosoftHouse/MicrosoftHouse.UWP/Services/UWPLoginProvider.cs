6ab77ac0571e34f02512050157e09eb8e253b8e2
Emanuele Torelli <mennetorelli@hotmail.it>
2017-04-14 21:38:41 +0200

    UWPLoginProvider

---
+++ App/MicrosoftHouse/MicrosoftHouse.UWP/Services/UWPLoginProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using MicrosoftHouse;
+using System.Threading.Tasks;
+
+namespace TaskList.UWP.Services
+{
+    class UWPLoginProvider : ILoginProvider
+    {
+        public async Task LoginAsync(MobileServiceClient client)
+        {
+            await client.LoginAsync("facebook");
+        }
+    }
+}
