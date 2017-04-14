074df678423409ac13473ea238d1e44903271ff9
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-04-14 09:43:37 +0200

    BE

--- App/Backend/App_Start/Startup.MobileApp.cs
+++ App/Backend/App_Start/Startup.MobileApp.cs
@@ -44,7 +44,7 @@ namespace Backend
         }
     }
 
-    public class MobileServiceInitializer : DropCreateDatabaseAlways<MobileServiceContext>
+	public class MobileServiceInitializer : CreateDatabaseIfNotExists<MobileServiceContext>
     {
         protected override void Seed(MobileServiceContext context)
         {
