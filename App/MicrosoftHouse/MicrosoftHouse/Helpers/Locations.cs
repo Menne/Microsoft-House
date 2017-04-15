8ccf9dce666856c2a1f1d802f8c17aab6a530179
FilippoPedrazzini <filippo.pedrazzini@yahoo.it>
2017-04-15 16:23:02 +0200

    BE

--- App/MicrosoftHouse/MicrosoftHouse/Helpers/Locations.cs
+++ App/MicrosoftHouse/MicrosoftHouse/Helpers/Locations.cs
@@ -3,14 +3,14 @@ namespace MicrosoftHouse
 {
 	public static class Locations
 	{
-		public static readonly string AppServiceUrl = "https://microsofthouse.azurewebsites.net";
+		//public static readonly string AppServiceUrl = "https://microsofthouse.azurewebsites.net";
 
-		public static readonly string AadClientId = "2f9bdb50-ffff-43c5-9331-42785326676e";
+		//public static readonly string AadClientId = "2f9bdb50-ffff-43c5-9331-42785326676e";
 
-		public static readonly string AadRedirectUri = "https://microsofthouse.azurewebsites.net";
+		//public static readonly string AadRedirectUri = "https://microsofthouse.azurewebsites.net";
 
-		public static readonly string AadAuthority = "https://login.windows.net/mhappoutlook.onmicrosoft.com";
+		//public static readonly string AadAuthority = "https://login.windows.net/mhappoutlook.onmicrosoft.com";
 
-		//public static readonly string AppServiceUrl = "http://microsofthouseadminpoli.azurewebsites.net/";
+		public static readonly string AppServiceUrl = "http://microsofthouseadminpoli.azurewebsites.net/";
 	}
 }
