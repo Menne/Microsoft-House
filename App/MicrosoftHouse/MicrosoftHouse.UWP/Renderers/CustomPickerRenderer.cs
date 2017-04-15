905a6d981695eb84bd032d83e537d44c1caa9f6f
Emanuele Torelli <mennetorelli@hotmail.it>
2017-04-15 14:33:42 +0200

    fix vari

---
+++ App/MicrosoftHouse/MicrosoftHouse.UWP/Renderers/CustomPickerRenderer.cs
@@ -0,0 +1,22 @@
+﻿using MicrosoftHouse.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using MicrosoftHouse;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+namespace MicrosoftHouse.UWP
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BorderThickness = new Windows.UI.Xaml.Thickness(0);
+            }
+
+        }
+    }
+}
