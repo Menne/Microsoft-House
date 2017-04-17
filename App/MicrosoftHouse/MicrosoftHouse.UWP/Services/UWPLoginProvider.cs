d827221d8d08cb1af0168fe1c66adf2fcfcaddcf
Emanuele Torelli <mennetorelli@hotmail.it>
2017-04-17 18:15:11 +0200

    configurato uwp

--- App/MicrosoftHouse/MicrosoftHouse.UWP/Services/UWPLoginProvider.cs
+++ App/MicrosoftHouse/MicrosoftHouse.UWP/Services/UWPLoginProvider.cs
@@ -2,15 +2,22 @@
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 using TaskList.UWP.Services;
+using System;
 
 [assembly: Xamarin.Forms.Dependency(typeof(UWPLoginProvider))]
 namespace TaskList.UWP.Services
 {
     public class UWPLoginProvider : ILoginProvider
     {
+
         public async Task LoginAsync(MobileServiceClient client)
         {
             await client.LoginAsync("facebook");
+        }
+
+        public string GetSyncStore()
+        {
+            throw new NotImplementedException();
         }
     }
 }
