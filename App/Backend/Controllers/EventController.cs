c72afcc251cd611f6c6d6684b46bab68a6f4e069
Emanuele Torelli <mennetorelli@hotmail.it>
2017-07-16 23:30:32 +0200

    Backend predisposto per notifiche

--- App/Backend/Controllers/EventController.cs
+++ App/Backend/Controllers/EventController.cs
@@ -6,6 +6,9 @@ using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Backend.Models;
 using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Azure.NotificationHubs;
+using System.Collections.Generic;
 
 namespace Backend
 {
@@ -31,22 +34,55 @@ namespace Backend
 		}
 
 		// PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-		public Task<Event> PatchEvent(string id, Delta<Event> patch)
+		public async Task<Event> PatchEvent(string id, Delta<Event> patch)
 		{
-			return UpdateAsync(id, patch);
-		}
+			var item = await UpdateAsync(id, patch);
+            await PushToSyncAsync("event", item.Id);
+            return item;
+        }
 
-		// POST tables/TodoItem
-		public async Task<IHttpActionResult> PostEvent(Event currentEvent)
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostEvent(Event currentEvent)
 		{
 			Event current = await InsertAsync(currentEvent);
-			return CreatedAtRoute("Tables", new { id = current.Id }, current);
+            await PushToSyncAsync("event", currentEvent.Id);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
 		}
 
 		// DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-		public Task DeleteEvent(string id)
+		public async Task DeleteEvent(string id)
 		{
-			return DeleteAsync(id);
-		}
-	}
+            await PushToSyncAsync("event", id);
+            await DeleteAsync(id);
+        }
+
+
+        private async Task PushToSyncAsync(string table, string id)
+        {
+            var appSettings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            var nhName = appSettings.NotificationHubName;
+            var nhConnection = appSettings.Connections[MobileAppSettingsKeys.NotificationHubConnectionString].ConnectionString;
+
+            // Create a new Notification Hub client
+            var hub = NotificationHubClient.CreateClientFromConnectionString(nhConnection, nhName);
+
+            // Create a template message
+            var templateParams = new Dictionary<string, string>();
+            templateParams["op"] = "sync";
+            templateParams["table"] = table;
+            templateParams["id"] = id;
+
+            // Send the template message
+            try
+            {
+                var result = await hub.SendTemplateNotificationAsync(templateParams);
+                Configuration.Services.GetTraceWriter().Info(result.State.ToString());
+            }
+            catch (Exception ex)
+            {
+                Configuration.Services.GetTraceWriter().Error(ex.Message, null, "PushToSync Error");
+            }
+        }
+
+    }
 }
